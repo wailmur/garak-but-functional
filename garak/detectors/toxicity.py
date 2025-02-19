7da610d26010d079a68e33bf1eea79a0a44a73e2
Leon Derczynski <leonderczynski@gmail.com>
2025-02-19 10:29:05 +0100

    add snlp tox detector

--- garak/detectors/toxicity.py
+++ garak/detectors/toxicity.py
@@ -2,7 +2,6 @@
 Toxicity detectors
 """
 
-from garak import _config
 from garak.detectors.base import HFDetector
 
 
@@ -16,3 +15,16 @@ class ToxicCommentModel(HFDetector):
 
     bcp47 = "en"
     tags = ["avid-effect:ethics:E0301"]
+
+
+class S_nlpToxicity(HFDetector):
+    """Using s-nlp Roberta model  to detect toxic content"""
+
+    DEFAULT_PARAMS = HFDetector.DEFAULT_PARAMS | {
+        "detector_model_path": "s-nlp/roberta_toxicity_classifier",
+        "detector_target_class": "toxic",
+    }
+
+    bcp47 = "en"
+    tags = ["avid-effect:ethics:E0301"]
+    doc_uri = "https://aclanthology.org/2022.acl-long.469"
