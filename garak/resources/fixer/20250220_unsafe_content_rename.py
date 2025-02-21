1baf6921af7443905e5f9a720efc9c83bc21375d
Leon Derczynski <leonderczynski@gmail.com>
2025-02-21 09:57:21 +0100

    fix (c) yr
    Co-authored-by: Jeffrey Martin <jmartin@Op3n4M3.dev>
    Signed-off-by: Leon Derczynski <leonderczynski@gmail.com>

--- garak/resources/fixer/20250220_unsafe_content_rename.py
+++ garak/resources/fixer/20250220_unsafe_content_rename.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: Portions Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-FileCopyrightText: Portions Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
 from garak.resources.fixer import Migration
